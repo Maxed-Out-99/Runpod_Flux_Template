2589f5fd4d4b69780f9929bf813e72b1a02d81f9
Maxed-Out-99 <maxedout99999@gmail.com>
2025-07-04 19:05:03 -0700

    Revamp

--- launch.sh
+++ launch.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "🔥 Running Maxed Out Installer..."
 cd /workspace/ComfyUI
-python3 /workspace/install_maxedout.py
 
-echo "🚀 Starting ComfyUI..."
-python3 main.py --listen 0.0.0.0 --port 8188 --enable-cors
+exec python3 main.py --listen --port 8188
